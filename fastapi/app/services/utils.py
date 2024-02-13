68817d46c0d3b1316f71400bd1984bc155885226
Richard Chai <atsuishisen@gmail.com>
2024-02-13 22:48:08 +0800

    Add files via upload

--- fastapi/app/services/utils.py
+++ fastapi/app/services/utils.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from upstash_redis import Redis
 from app.models.Polls import Poll
+from uuid import UUID
+from typing import Optional
 
 load_dotenv()
 REDIS_REST_API_URL = os.getenv('KV_REST_API_URL')
@@ -10,5 +12,17 @@ redis_client = Redis(url=REDIS_REST_API_URL, token=REDIS_REST_API_TOKEN)
 
 
 def save_poll(poll: Poll):
-    redis_client.set(f"poll: {poll.id}", poll.model_dump_json())
+    poll_json = poll.model_dump_json()
+    redis_client.set(f"poll_{poll.id}", poll_json)
 
+
+def get_poll(poll_id: UUID) -> Optional[Poll]:
+    poll_json = redis_client.get(f"poll_{poll_id}")
+
+    print(f"poll_json: {type(poll_json)} *** \n{poll_json} \n*******")
+
+    if poll_json:
+
+        return Poll.model_validate_json(poll_json)
+    else:
+        return None
