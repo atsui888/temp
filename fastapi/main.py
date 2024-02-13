89122fffcab2fb7c99877fff8d29663983345f38
Richard Chai <atsuishisen@gmail.com>
2024-02-13 22:47:39 +0800

    Add files via upload

--- fastapi/main.py
+++ fastapi/main.py
@@ -5,9 +5,12 @@
 
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI  #, HTTPException
+from fastapi import FastAPI, HTTPException
 from upstash_redis import Redis
 from app.models.Polls import Poll, PollCreate
+from app.services import utils
+from uuid import UUID
+
 
 load_dotenv()  # set environment variables from .env.
 REDIS_REST_API_URL = os.getenv('KV_REST_API_URL')
@@ -30,6 +33,7 @@ def create_poll(poll: PollCreate):
     # rather than using a extra object "PollCreate" to create a "Poll" object. If the reason for this way of doing
     # things is to save on code lines, then maybe using composition is better ??
     new_poll = poll.create_poll()
+    utils.save_poll(new_poll)
     return {
         "detail": "Poll successfully created.",
         "poll_id": new_poll.id,
@@ -37,16 +41,27 @@ def create_poll(poll: PollCreate):
     }
 
 
+@app.get("/polls/{poll_id}")
+def get_poll(poll_id: UUID):
+    poll = utils.get_poll(poll_id)
+    if not poll:
+        raise HTTPException(
+            status_code=404,
+            detail=f"A poll with id: '{poll_id}' is not found.")
+    else:
+        return poll
+
+
 # exploration code - to be deleted later
-# redis_client = Redis(url=REDIS_REST_API_URL, token=REDIS_REST_API_TOKEN)
-#
-#
-# @app.post("/redis/save", tags=["throwaway"])
-# def save_redis(uid: str, name: str):
-#     redis_client.set(uid, name)
-#     return {"status": "success"}
-#
-#
-# @app.get("/redis/get/{uid}")
-# def get_redis(uid: str):
-#     return {"uid": uid, "name": redis_client.get(uid)}
+redis_client = Redis(url=REDIS_REST_API_URL, token=REDIS_REST_API_TOKEN)
+
+
+@app.post("/redis/save", tags=["throwaway"])
+def save_redis(uid: str, name: str):
+    redis_client.set(uid, name)
+    return {"status": "success"}
+
+
+@app.get("/redis/get/{uid}")
+def get_redis(uid: str):
+    return {"uid": uid, "name": redis_client.get(uid)}
